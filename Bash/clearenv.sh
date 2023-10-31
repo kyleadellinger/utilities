0568e1da64e118568bb20bf86301c311e4f6e774
kyleadellinger <116576528+kyleadellinger@users.noreply.github.com>
2023-10-31 19:16:13 -0400

    Create clearenv.sh

---
+++ Bash/clearenv.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+## clear environment variables
+
+function clearenv () {
+    unset WS_USERKEY
+    unset WS_EMAIL
+    unset WS_WSS_URL
+    unset WS_APIKEY
+    unset API_URL
+    unset API2_URL
+}
+clearenv
