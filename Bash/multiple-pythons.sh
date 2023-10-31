797d4d1e8ff8dea4b360362d21c5f1034741de3a
kyleadellinger <116576528+kyleadellinger@users.noreply.github.com>
2023-10-31 19:33:40 -0400

    Update multiple-pythons.sh

--- Bash/multiple-pythons.sh
+++ Bash/multiple-pythons.sh
@@ -3,3 +3,7 @@
 sudo apt install software-properties-common -y
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt install python3.9 -y
+
+# sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.9 1
+# sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.10 2
+# etc 
