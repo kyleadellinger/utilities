9fe81edbe45813aed31a6d3b0b3ff038a67f4edc
kyleadellinger <116576528+kyleadellinger@users.noreply.github.com>
2024-09-09 11:06:54 -0400

    Update kargs.py

--- Python/argparse_notes/kargs.py
+++ Python/argparse_notes/kargs.py
@@ -1,1 +1,41 @@
 
+import argparse
+
+## CLI INTERFACE ##
+parser = argparse.ArgumentParser(description="the thing this whole thing does")
+
+# positional, mandatory
+parser.add_argument("echo", help="echo the string you use here")
+
+# optional; make flag
+parser.add_argument("-v", "--verbosity",
+                    help="increase output verbosity",
+                    action="store_true")
+
+# parameter validation
+parser.add_argument("-p", "--print",
+                    help="print 1|2",
+                    type=int, choices=[0,1,2])
+
+# count; -t vs -tt
+parser.add_argument("-t", "--typer",
+                    help="typing",
+                    action="count")
+
+# mutually exclusive group
+group = argparse.ArgumentParser()
+group = parser.add_mutually_exclusive_group()
+group.add_argument("-k", "--okay", action="store_true")
+group.add_argument("-q", "--quiet", action="store_true")
+
+
+args = parser.parse_args()
+
+## LOGIC ## 
+if args.verbosity:
+    print("verbosity turned on")
+
+if args.typer == 1:
+    print("number 1")
+elif args.typer == 2:
+    print("number 2 number 2")
