944aff7977defabd77efa334a98494a13a8fdf39
Kyle Dellinger <kdellinger@webstaurantstore.com>
2026-04-16 13:50:29 -0400

    winrm https self-signed pwsh notes

---
+++ PowerShell/Do-WinRM-HTTPS.ps1
@@ -0,0 +1,30 @@
+
+## these are rought notes to:
+## - generate a self-signed certificate
+## - enable ps remoting
+## - create relevant firewall rule (HTTPS-In)
+
+## note: in testing this has worked with supplying IP address in place of DNS/computer hostname
+
+$certificate = New-SelfSignedCertificate -CertStoreLocation 'Cert:\LocalMachine\My' -DnsName 'ipaddress'
+
+Enable-PSRemoting -SkipNetworkProfileCheck -Force
+
+New-Item -Path WSMan:\LocalHost\Listener `
+    -Transport HTTPS `
+    -Address * `
+    -CertificateThumbprint $($certificate.thumbprint) `
+    -Force
+
+New-NetFirewallRule -DisplayName 'Windows Remote Management (HTTPS-In)' `
+    -Name 'Windows Remote Management (HTTPS-In)' `
+    -Profile Any `
+    -LocalPort 5986 `
+    -Protocol TCP
+
+## connect example for notes. didn't seem to be required, at least from ansible connection
+# Enter-PSSession -ComputerName '' `
+#   -Credential (Get-Credential) `
+#   -SessionOption (New-PSSessionOption -SkipCACheck -SnipCNCheck) `
+#   -UseSsl
+
